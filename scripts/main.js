2a41d5cfff323fa019a17e5abee8e811d72ee104
Yannick Kok <37619401+YannickKok@users.noreply.github.com>
2019-03-11 17:07:10 +0100

    var seconds

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-TweenMax.to("#analog", 8, {
+TweenMax.to("#analog", 86400, {
   rotation: 360,
   ease:Linear.easeNone,
   repeat:-1})
@@ -25,7 +25,15 @@ var myVar = setInterval(function() {
      ' / ' + (maanden[today.getMonth()]) +
       ' / ' + today.getFullYear(); 
 
-
+      var hms = ('0' + today.getHours()).slice(-2) + 
+      ' : ' + ('0' + (today.getMinutes())).slice(-2) +
+       ' : ' + ('0' + (today.getSeconds())).slice(-2);   // your input string
+      var a = hms.split(':'); // split it at the colons
+      
+      // minutes are worth 60 seconds. Hours are worth 60 minutes.
+      var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
+      
+      console.log(seconds);
 
 
 }
