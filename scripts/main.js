9adcce2460556cf8ef09663e1dd88f6017a0ba48
Yannick Kok <37619401+YannickKok@users.noreply.github.com>
2019-03-11 21:21:28 +0100

    javascript werkt

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,4 @@
-TweenMax.to("#analog", 8, {
-  rotation: 360,
-  ease:Linear.easeNone,
-  repeat:-1})
+
 
 var maanden = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -10,6 +7,7 @@ var myVar = setInterval(function() {
   }, 1000);
 
   var timeline = new TimelineMax({repeat: -1});
+  
   timeline.call(myTimer)
   .to('#clock', 1, { left: 65 ,repeat: -1});
 
@@ -32,9 +30,18 @@ var myVar = setInterval(function() {
       
       // minutes are worth 60 seconds. Hours are worth 60 minutes.
       var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
-      
-      console.log(seconds);
+      var secondsdegrees = Math.round(seconds/240);
 
+      console.log(secondsdegrees);
+
+      TweenMax.set("#analog", {rotation: secondsdegrees})
+      
+      TweenMax.to("#analog", 86400,  {
+        
+        rotation: 360,
+        
+        ease:Linear.easeNone,
+        repeat:-1})
 
 }
 
