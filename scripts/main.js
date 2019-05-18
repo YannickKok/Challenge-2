a32a8ebc2f7264dda73e91b7bbd422b106141560
Yannick Kok <37619401+YannickKok@users.noreply.github.com>
2019-05-18 13:12:12 +0200

    Update 1
    Tijden aflezen gebeurd nu uit 1 JS file. Tijden blijven hierdoor doorlopen.
    Animatie iets verkleint.
    Tijdzones aangepast waardoor alle 4 de tijdzones nu werken.
    Nu nog tijden koppelen aan de animaties van de aardbol.

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,23 @@
 
 var maanden = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; //Engelse maanden want SpaceX spreekt engels
 
-var myVar = setInterval(function() {
+var myVar = setInterval(function() { 
     myTimer();
-  }, 1000);
+  }, 1000); //Interval van 1000 MS zorgt ervoor dat er 1 update  is per 1 seconde.
 
-  var timeline = new TimelineMax({repeat: -1, })
+  var timeline = new TimelineMax({repeat: -1, }) // Repeat -1 voor infinite loop.
   
   
   
   timeline.call(myTimer)
-  .to('#clock', 1, { left: 65 ,repeat: -1, yoyo:true, ease:Linear.easeNone,}); //Gekke animatie om de depressieve mars kolonisten op te vrolijken
+  .to('#clock, #clock2, #clock3, #clock4',  1, { left: 35 ,repeat: -1, yoyo:true, ease:Linear.easeNone,}) //Gekke animatie om de depressieve mars kolonisten op te vrolijken.
+  
+  
+  
 
   function myTimer() {
     var today = new Date();
+    var options = { timeZone: "America/New_York", timeZoneName: 'short', hour12:false };
     
     document.getElementById('clock').innerHTML = ('0' + today.getHours()).slice(-2) +  //'0' + .slice(-2) zorgt er voor dat er leading zero's komen
     ' : ' + ('0' + (today.getMinutes())).slice(-2) +
@@ -28,6 +32,50 @@ var myVar = setInterval(function() {
       ' : ' + ('0' + (today.getMinutes())).slice(-2) +
        ' : ' + ('0' + (today.getSeconds())).slice(-2);   
       var a = hms.split(':'); 
+
+      //CLOCK 2
+
+      document.getElementById('clock2').innerHTML = ('0' + today.getUTCHours()).slice(-2) +  //'0' + .slice(-2) zorgt er voor dat er leading zero's komen
+      ' : ' + ('0' + (today.getUTCMinutes())).slice(-2) +
+       ' : ' + ('0' + (today.getUTCSeconds())).slice(-2);
+  
+      document.getElementById('date2').innerHTML = ('0' + today.getUTCDate()).slice(-2) +
+       ' / ' + (maanden[today.getUTCMonth()]) +
+        ' / ' + today.getUTCFullYear(); 
+  
+        var hms = ('0' + today.getUTCHours()).slice(-2) + 
+        ' : ' + ('0' + (today.getUTCMinutes())).slice(-2) +
+         ' : ' + ('0' + (today.getUTCSeconds())).slice(-2);   
+        var a = hms.split(':'); 
+
+        //CLOCK3
+            
+    document.getElementById('clock3').innerHTML = today.toLocaleTimeString('en-US', options)
+
+    document.getElementById('date3').innerHTML = today.toLocaleDateString('en-US', options)
+
+      var hms = today.toLocaleTimeString('en-US', options)
+      var a = hms.split(':'); 
+    
+
+
+      var hms = today.getUTCHours() + 
+      ' : ' + today.getUTCMinutes() +
+       ' : ' + today.getUTCSeconds();    
+      var a = hms.split(':'); 
+
+      //CLOCK4
+      var options = { timeZone: "America/Los_Angeles", timeZoneName: 'short', hour12:false };      
+    document.getElementById('clock4').innerHTML = today.toLocaleTimeString('en-US', options)
+
+    document.getElementById('date4').innerHTML = today.toLocaleDateString('en-US', options)
+
+      var hms = ('0' + today.getHours()).slice(-2) + 
+      ' : ' + ('0' + (today.getMinutes())).slice(-2) +
+       ' : ' + ('0' + (today.getSeconds())).slice(-2);   
+      var a = hms.split(':'); 
+
+        
       
    
       var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);    // Uren zijn 60 minuten. Minuten zijn 60 seconden. Rekenwerk zodat alles naar seconden geconvert kan worden.
